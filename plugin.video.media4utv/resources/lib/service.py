d9239e67e7ff508a6f5d3e6c9371476f2607dcee
tdbnz-coding <49893160+tdbnz-coding@users.noreply.github.com>
2025-03-18 17:41:36 +1300

    update

--- plugin.video.media4utv/resources/lib/service.py
+++ plugin.video.media4utv/resources/lib/service.py
@@ -1,16 +1,20 @@
 import xbmc
 import xbmcaddon
 import time
-from lib.xtream_api import XtreamAPI
-from resources.lib.cache import CacheManager
+from resources.lib.xtream_api import XtreamAPI  # ✅ Correct path
+from resources.lib.cache import CacheManager  # ✅ Correct path
 
+# Initialize Add-on
 ADDON = xbmcaddon.Addon()
 BASE_URL = "http://m3ufilter.media4u.top/player_api.php"
 USERNAME = ADDON.getSetting("username")
 PASSWORD = ADDON.getSetting("password")
+
+# Initialize API and Cache
 API = XtreamAPI(BASE_URL, USERNAME, PASSWORD)
 CACHE = CacheManager()
 
+# Background process to refresh cache every hour
 while not xbmc.abortRequested:
     CACHE.update("categories", API.get_live_categories)
     xbmc.sleep(3600000)  # Refresh every 1 hour
